0101dd2a9f92d7cc225f76e52dddbe9ce8b2b2b5
Harsh Saini <kukharsh144@example.com>
2026-08-11 17:29:41 +0530

    in this we complete the video 12 in we use postman to check our api of backend is working properly ar not      we add routers in that for the first  and a simle controller one of the one thing is that we forgott to  import the app.js in index.js that make tthe connectioon failure and we want that we have to check that else everything going working

--- src/app.js
+++ src/app.js
@@ -14,4 +14,13 @@ app.use(express.json({limit: '16kb'}));
 app.use(express.urlencoded({ extended: true , limit: '16kb' }));
 app.use(express.static('public'));
 
+
+// =====================  import router ==============================
+
+import userRouters from "./routes/user.routes.js";
+
+app.use("/api/v1/users", userRouters);
+
+
+
 export { app };

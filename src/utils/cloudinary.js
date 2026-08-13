903bb9f651f6d50c9387bc0310f498e5f2ca236b
Harsh Saini <kukharsh144@example.com>
2026-08-13 11:47:42 +0530

    done

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -11,7 +11,8 @@ const uploadOnCloudinary = async (localFilePath) => {
     try {
         if (!localFilePath) return null;
         const response = await cloudinary.uploader.upload(localFilePath, { resource_type : "auto"});
-        console.log(response.url);
+        // console.log(response.url);
+        fs.unlinkSync(localFilePath);  
         return response;
     } catch(error) {
         fs.unlinkSync(localFilePath);  

037560d6318d1a81c108ee1b4d1dd2d610b887e0
Harsh Saini <kukharsh144@example.com>
2026-08-18 12:37:30 +0530

    Fix typo in cors configuration for credentials option in app.js

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(cors({
     origin : process.env.CORS_ORIGIN,
-    Credentials : true
+    credentials : true
 }));
 app.use(cookieParser());
 app.use(express.json({limit: '16kb'}));
